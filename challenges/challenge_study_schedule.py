4137aaa033d602140a95d9f1c4ae9d094d196827
Camila Wolter <camila0wolter@gmail.com>
2023-04-25 14:56:11 -0300

    feat: criando algoritmo de busca de número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        count = 0
+        for start_time, end_time in permanence_period:
+            if start_time <= target_time <= end_time:
+                count += 1
+        return count
+    except TypeError:
+        return None
