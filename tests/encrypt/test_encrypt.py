3783c40af898bc2e4bb67a39b338e5133b5d540f
Camila Wolter <camila0wolter@gmail.com>
2023-04-25 15:13:08 -0300

    test: criptografia de inversões

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(4, "teste")
+
+    assert encrypt_message("teste", 4) == "e_tset"
+
+    assert encrypt_message("teste", 3) == "set_et"
+
+    assert encrypt_message("112", 0) == "211"
